587e8daa38e89f69cec467a4e3190013bf5b936c
Peter Palaga <ppalaga@redhat.com>
2017-11-21 22:00:12 +0100

    Put SequenceFinder inside LCSS as that's the only place where it is used, hide what is hideable

--- services/src/main/java/org/ec4j/core/services/completion/LCSS.java
+++ services/src/main/java/org/ec4j/core/services/completion/LCSS.java
@@ -16,13 +16,152 @@
  */
 package org.ec4j.core.services.completion;
 
-import java.util.List;
+import static java.lang.Character.isLetter;
+import static java.lang.Character.isLowerCase;
+import static java.lang.Character.isUpperCase;
+import static java.lang.Character.toLowerCase;
+import static java.lang.Character.toUpperCase;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 /**
  * Copied from
  * https://github.com/eclipse/recommenders/blob/master/plugins/org.eclipse.recommenders.subwords.rcp/src/org/eclipse/recommenders/internal/subwords/rcp/LCSS.java
  */
-public final class LCSS {
+final class LCSS {
+
+    /**
+     * Copied from
+     * https://github.com/eclipse/recommenders/blob/master/plugins/org.eclipse.recommenders.subwords.rcp/src/org/eclipse/recommenders/internal/subwords/rcp/SequenceFinder.java
+     *
+     */
+    static class SequenceFinder {
+
+        private static final int[] EMPTY_SEQUENCE = new int[0];
+
+        private List<int[]> curSequences = new LinkedList<int[]>(); // Lists.newLinkedList();
+        private List<int[]> nextSequences = new LinkedList<int[]>(); // Lists.newLinkedList();
+
+        private int pCompletion, pToken;
+        private String completion, token;
+
+        SequenceFinder(String completion, String token) {
+            this.completion = completion;
+            this.token = token;
+        }
+
+        List<int[]> findSeqeuences() {
+
+            if (isConstantName(completion)) {
+                rewriteCompletion();
+            }
+
+            int[] start = EMPTY_SEQUENCE;
+            curSequences.add(start);
+
+            for (pToken = 0; pToken < token.length(); pToken++) {
+                char t = token.charAt(pToken);
+
+                for (int[] activeSequence : curSequences) {
+                    boolean mustmatch = false;
+                    int startIndex = activeSequence.length == 0 ? 0 : activeSequence[activeSequence.length - 1] + 1;
+
+                    for (pCompletion = startIndex; pCompletion < completion.length(); pCompletion++) {
+                        char c = completion.charAt(pCompletion);
+                        if (!Character.isLetter(c)) {
+                            if (c == t) {
+                                addNewSubsequenceForNext(activeSequence);
+                                continue;
+                            }
+                            mustmatch = true;
+                            continue;
+                        } else if (Character.isUpperCase(c)) {
+                            mustmatch = true;
+                        }
+
+                        if (mustmatch && !isSameIgnoreCase(c, t)) {
+                            jumpToEndOfWord();
+                        } else if (isSameIgnoreCase(c, t)) {
+                            addNewSubsequenceForNext(activeSequence);
+                        }
+                    }
+                }
+                curSequences = nextSequences;
+                nextSequences = new LinkedList<int[]>(); // Lists.newLinkedList();
+            }
+
+            // filter
+            for (Iterator<int[]> it = curSequences.iterator(); it.hasNext();) {
+                int[] candidate = it.next();
+                if (candidate.length < token.length()) {
+                    it.remove();
+                    continue;
+                }
+            }
+
+            return curSequences;
+        }
+
+        private void addNewSubsequenceForNext(int[] activeSequence) {
+            int[] copy = Arrays.copyOf(activeSequence, activeSequence.length + 1);
+            copy[pToken] = pCompletion;
+            nextSequences.add(copy);
+        }
+
+        private void rewriteCompletion() {
+            StringBuilder sb = new StringBuilder();
+
+            boolean toUpperCase = false;
+            for (char c : completion.toCharArray()) {
+                if (Character.isLetterOrDigit(c)) {
+                    sb.append(toUpperCase ? Character.toUpperCase(c) : Character.toLowerCase(c));
+                    toUpperCase = false;
+                } else {
+                    sb.append(c);
+                    toUpperCase = true;
+                }
+            }
+            completion = sb.toString();
+        }
+
+        private void jumpToEndOfWord() {
+            for (pCompletion++; pCompletion < completion.length(); pCompletion++) {
+                char next = completion.charAt(pCompletion);
+
+                if (!isLetter(next)) {
+                    // . or _ word boundary found:
+                    // XXX numbers are also considered as word boundaries then. this
+                    // may cause some trouble...
+                    break;
+                }
+
+                if (isUpperCase(next)) {
+                    pCompletion--;
+                    break;
+                }
+            }
+        }
+
+        private boolean isConstantName(String completion) {
+            for (char c : completion.toCharArray()) {
+                if (Character.isLetter(c) && Character.isLowerCase(c)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private boolean isSameIgnoreCase(char c1, char c2) {
+            if (c1 == c2) {
+                return true;
+            }
+            c2 = isLowerCase(c2) ? toUpperCase(c2) : toLowerCase(c2);
+            return c1 == c2;
+        }
+
+    }
 
     private LCSS() {
         // Not meant to be instantiated
@@ -34,7 +173,7 @@ public final class LCSS {
      * Returns the best, i.e, the longest continuous sequence - or the empty
      * sequence if no subsequence could be found.
      */
-    public static int[] bestSubsequence(String completion, String token) {
+    static int[] bestSubsequence(String completion, String token) {
         int bestScore = -1;
         int[] bestSequence = EMPTY_SEQUENCE;
         for (int[] s1 : findSequences(completion, token)) {
@@ -47,7 +186,7 @@ public final class LCSS {
         return bestSequence;
     }
 
-    public static int scoreSubsequence(int[] s1) {
+    static int scoreSubsequence(int[] s1) {
         int score = 0;
         for (int i = 0; i < s1.length - 1; i++) {
             if (s1[i] + 1 == s1[i + 1]) {
@@ -57,11 +196,11 @@ public final class LCSS {
         return score;
     }
 
-    public static List<int[]> findSequences(String completion, String token) {
+    static List<int[]> findSequences(String completion, String token) {
         return new SequenceFinder(completion, token).findSeqeuences();
     }
 
-    public static boolean containsSubsequence(String completion, String token) {
+    static boolean containsSubsequence(String completion, String token) {
         return !findSequences(completion, token).isEmpty();
     }
 }
